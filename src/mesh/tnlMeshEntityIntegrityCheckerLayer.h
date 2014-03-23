fb8834674876143188cdc030224b6c223fe4a790
Tomas Oberhuber <tomas.oberhuber@fjfi.cvut.cz>
2014-03-23 19:04:45 +0100

    Implementing the mesh integrity checker.

---
+++ src/mesh/tnlMeshEntityIntegrityCheckerLayer.h
@@ -0,0 +1,24 @@
+/***************************************************************************
+                          tnlMeshEntityIntegrityCheckerLayer.h  -  description
+                             -------------------
+    begin                : Mar 23, 2014
+    copyright            : (C) 2014 by Tomas Oberhuber
+    email                : tomas.oberhuber@fjfi.cvut.cz
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef TNLMESHENTITYINTEGRITYCHECKERLAYER_H_
+#define TNLMESHENTITYINTEGRITYCHECKERLAYER_H_
+
+
+
+
+#endif /* TNLMESHENTITYINTEGRITYCHECKERLAYER_H_ */
