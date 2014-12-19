80df7a87082c60f16b2135831bf546f60568f6f0
Tomas Oberhuber <tomas.oberhuber@fjfi.cvut.cz>
2014-12-19 15:57:30 +0100

    Adding tnlMatrixRow.

---
+++ src/implementation/matrices/tnlMatrixRow_impl.h
@@ -0,0 +1,24 @@
+/***************************************************************************
+                          tnlMatrixRow_impl.h  -  description
+                             -------------------
+    begin                : Dec 19, 2014
+    copyright            : (C) 2014 by Tomas Oberhuber
+    email                : tomas.oberhuber@fjfi.cvut.cz
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef TNLMATRIXROW_IMPL_H_
+#define TNLMATRIXROW_IMPL_H_
+
+
+
+
+#endif /* TNLMATRIXROW_IMPL_H_ */
---
+++ src/matrices/tnlMatrixRow.h
@@ -0,0 +1,49 @@
+/***************************************************************************
+                          tnlMatrixRow.h  -  description
+                             -------------------
+    begin                : Dec 19, 2014
+    copyright            : (C) 2014 by Tomas Oberhuber
+    email                : tomas.oberhuber@fjfi.cvut.cz
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#ifndef TNLMATRIXROW_H_
+#define TNLMATRIXROW_H_
+
+template< typename Real, typename Index >
+class tnlMatrixRow
+{
+   public:
+
+      tnlMatrixRow();
+
+      void bind( Index* columns,
+                 Real* values,
+                 const Index length,
+                 const Index step );
+
+      Real setElement( const Index& elementIndex,
+                       const Index& column,
+                       const Real& value );
+
+   protected:
+
+      Real* values;
+
+      Index* columns;
+
+      Index length, step;
+};
+
+#include <implementation/matrices/tnlMatrixRow_impl.h>
+
+#endif /* TNLMATRIXROW_H_ */
