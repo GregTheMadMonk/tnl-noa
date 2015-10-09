41daa87661cfe826d99148a386a809a9e18d437c
Jakub Klinkovský <klinkjak@fjfi.cvut.cz>
2015-10-09 18:24:22 +0200

    Avoid compiler warnings generated by -Wall

--- src/solvers/ode/tnlEulerSolver_impl.h
+++ src/solvers/ode/tnlEulerSolver_impl.h
@@ -59,6 +59,7 @@ bool tnlEulerSolver< Problem > :: setup( const tnlParameterContainer& parameters
    tnlExplicitSolver< Problem >::setup( parameters, prefix );
    if( parameters.checkParameter( prefix + "euler-cfl" ) )
       this->setCFLCondition( parameters.getParameter< double >( prefix + "euler-cfl" ) );
+   return true;
 }
 
 template< typename Problem >
