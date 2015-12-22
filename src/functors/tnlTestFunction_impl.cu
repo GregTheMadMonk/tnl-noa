2bc54529ff0fd5f46fcf859d1cd2ce36d2bb3393
Jakub Klinkovský <klinkjak@fjfi.cvut.cz>
2015-12-22 22:59:11 +0100

    Added missing guards against explicit templates instantiation

--- src/functors/tnlTestFunction_impl.cu
+++ src/functors/tnlTestFunction_impl.cu
@@ -20,17 +20,21 @@
 
 #include <functors/tnlTestFunction.h>
 
+#ifdef INSTANTIATE_FLOAT
 template class tnlTestFunction< 1, float, tnlCuda >;
 template class tnlTestFunction< 2, float, tnlCuda >;
 template class tnlTestFunction< 3, float, tnlCuda >;
+#endif
 
 template class tnlTestFunction< 1, double, tnlCuda >;
 template class tnlTestFunction< 2, double, tnlCuda >;
 template class tnlTestFunction< 3, double, tnlCuda >;
 
-/*template class tnlTestFunction< 1, long double, tnlCuda >;
+#ifdef INSTANTIATE_LONG_DOUBLE
+template class tnlTestFunction< 1, long double, tnlCuda >;
 template class tnlTestFunction< 2, long double, tnlCuda >;
-template class tnlTestFunction< 3, long double, tnlCuda >;*/
+template class tnlTestFunction< 3, long double, tnlCuda >;
+#endif
 
 #endif
 #endif
