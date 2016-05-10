GregTheMadMonk/tnl-noa

commit 46dd704a092d85e12b7eff533a1040d8bb897ba3

Added timer for preconditioner update

src/solvers/pde/tnlSemiImplicitTimeStepper_impl.h
@@ -73,8 +73,6 @@ init( const MeshType& mesh )
    }
    if( ! this->rightHandSide.setSize( this->matrix.getRows() ) )
       return false;
-   this->linearSystemAssemblerTimer.reset();
-   this->linearSystemSolverTimer.reset();
    return true;
 }
 
@@ -177,8 +175,9 @@ solve( const RealType& time,
       if( verbose )
          cout << "                                                                  Solving the linear system for time " << t + currentTau << "             \r" << flush;
 
-      // TODO: add timer
+      this->preconditionerUpdateTimer.start();
       preconditioner.update( this->matrix );
+      this->preconditionerUpdateTimer.stop();
 
       this->linearSystemSolverTimer.start();
       if( ! this->linearSystemSolver->template solve< DofVectorType, tnlLinearResidueGetter< MatrixType, DofVectorType > >( this->rightHandSide, dofVector ) )
@@ -216,6 +215,7 @@ writeEpilog( tnlLogger& logger )
 {
    logger.writeParameter< double >( "Pre-iterate time:", this->preIterateTimer.getTime() );
    logger.writeParameter< double >( "Linear system assembler time:", this->linearSystemAssemblerTimer.getTime() );
+   logger.writeParameter< double >( "Preconditioner update time:", this->preconditionerUpdateTimer.getTime() );
    logger.writeParameter< double >( "Linear system solver time:", this->linearSystemSolverTimer.getTime() );
    logger.writeParameter< double >( "Post-iterate time:", this->postIterateTimer.getTime() );
    return true;

src/solvers/pde/tnlSemiImplicitTimeStepper.h
@@ -80,7 +80,7 @@ class tnlSemiImplicitTimeStepper
 
    RealType timeStep;
 
-   tnlTimerRT preIterateTimer, linearSystemAssemblerTimer, linearSystemSolverTimer, postIterateTimer;
+   tnlTimerRT preIterateTimer, linearSystemAssemblerTimer, preconditionerUpdateTimer, linearSystemSolverTimer, postIterateTimer;
    
    bool verbose;
 };

commit 63ee44852aef6cc18311c8fac2201d6d3445d09b

Fixed bug in tnlSemiImplicitTimeStepper.h
Caused by 46dd704a092d85e12b7eff533a1040d8bb897ba3

src/solvers/pde/tnlSemiImplicitTimeStepper_impl.h
@@ -230,7 +230,7 @@ writeEpilog( tnlLogger& logger )
    this->preIterateTimer.writeLog( logger, 1 );
    logger.writeParameter< const char* >( "Linear system assembler time:", "" );
    this->linearSystemAssemblerTimer.writeLog( logger, 1 );
-   logger.writeParameter< double >( "Preconditioner update time:", "" );
+   logger.writeParameter< const char* >( "Preconditioner update time:", "" );
    this->preconditionerUpdateTimer.writeLog( logger, 1 );
    logger.writeParameter< const char* >( "Linear system solver time:", "" );
    this->linearSystemSolverTimer.writeLog( logger, 1 );