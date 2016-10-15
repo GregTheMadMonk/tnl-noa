50f91836b56bc86cdb5fae2ce207770ee688a78e
Jakub Klinkovský <klinkjak@fjfi.cvut.cz>
2016-10-15 12:40:12 +0200

    Small changes in grids

--- src/TNL/Meshes/MeshDimensionsTag.h
+++ src/TNL/Meshes/MeshDimensionsTag.h
@@ -17,7 +17,7 @@ namespace Meshes {
 
 /***
  * This tag or integer wrapper is necessary for C++ templates specializations.
- * As th C++ standard says:
+ * As the C++ standard says:
  *
  *   A partially specialized non-type argument expression shall not involve
  *   a template parameter of the partial specialization except when the argument
@@ -26,7 +26,7 @@ namespace Meshes {
  * Therefore one cannot specialize the mesh layers just by integers saying the mesh
  * layer dimensions but instead this tag must be used. This makes the code more difficult
  * to read and we would like to avoid it if it is possible sometime.
- * On the other hand, tnlDimensionTag is also used for method overloading when
+ * On the other hand, MeshDimensionsTag is also used for method overloading when
  * asking for different mesh entities. In this case it makes sense and it cannot be
  * replaced.
  */
