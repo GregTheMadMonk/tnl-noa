e2f66b49a5326c313b2e221ab50ecf7d99dabe73
Tomas Oberhuber <tomas.oberhuber@fjfi.cvut.cz>
2017-02-24 20:35:18 +0100

    Lazy parameter at SharedPointer was removed.

--- src/TNL/Solvers/Linear/CWYGMRES.h
+++ src/TNL/Solvers/Linear/CWYGMRES.h
@@ -27,8 +27,8 @@ public:
    typedef typename Matrix::DeviceType DeviceType;
    typedef Matrix MatrixType;
    typedef Preconditioner PreconditionerType;
-   typedef SharedPointer< const MatrixType, DeviceType, true > MatrixPointer;
-   typedef SharedPointer< const PreconditionerType, DeviceType, true > PreconditionerPointer;
+   typedef SharedPointer< const MatrixType, DeviceType > MatrixPointer;
+   typedef SharedPointer< const PreconditionerType, DeviceType > PreconditionerPointer;
    typedef Containers::Vector< RealType, DeviceType, IndexType > DeviceVector;
    typedef Containers::Vector< RealType, Devices::Host, IndexType > HostVector;
 
