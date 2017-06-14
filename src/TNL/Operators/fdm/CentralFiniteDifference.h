a86794ca65c2738357794076dcd3c19c182bf272
Jakub Klinkovský <klinkjak@fjfi.cvut.cz>
2017-06-14 19:13:47 +0200

    Fixing operators and VectorField

--- src/TNL/Operators/fdm/CentralFiniteDifference.h
+++ src/TNL/Operators/fdm/CentralFiniteDifference.h
@@ -48,7 +48,7 @@ class CentralFiniteDifference< Meshes::Grid< Dimension, MeshReal, MeshDevice, Me
       typedef Index IndexType;
       typedef ExactDifference< Dimension, XDifference, YDifference, ZDifference > ExactOperatorType;
  
-      //static constexpr int getDimension() { return Dimension; }
+      //static constexpr int getMeshDimension() { return Dimension; }
  
       static String getType()
       {
