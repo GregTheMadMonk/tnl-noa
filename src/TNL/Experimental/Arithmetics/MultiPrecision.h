3e90725742d7919a2e3ec890b4fce66cc2b62195
Daniel Simon <mail@danielsimon.cz>
2017-10-22 18:33:44 +0200

    Files for MultiPrescision class added into ../Experimental/Arihmetics/

--- src/TNL/Experimental/Arithmetics/MultiPrecision.h
+++ src/TNL/Experimental/Arithmetics/MultiPrecision.h
@@ -1,1 +1,12 @@
+/**************************************************             
+* filename:             MultiPrecision.h          *
+* created:              October 22, 2017          *
+* author:               Daniel Simon              *
+* mail:                 dansimon93@gmail.com      *
+***************************************************/
+
+// TODO
+// Implementation of GNU MP Library
+// Operators overloading
+// Implementation of missing functions
 
