e86215c46ea7230c5e9f48b31bde05451d2c0cd8
Daniel Simon <mail@danielsimon.cz>
2017-12-06 21:56:45 +0100

    Development of double precision started. Files added into repository.

---
+++ src/TNL/Experimental/Arithmetics/Double_impl.h
@@ -0,0 +1,6 @@
+/**************************************************
+* filename:		Double_impl.h	          *
+* created:		December 6, 2017	  *
+* author:		Daniel Simon	 	  *
+* mail:			dansimon93@gmail.com      *
+***************************************************/
