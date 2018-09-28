db52de9e27338c1c7c6a59c33629b8671c36cdde
Jakub Klinkovský <klinkjak@fjfi.cvut.cz>
2018-09-28 16:02:23 +0200

    Fixed MPISupportMissing.h

--- src/TNL/Exceptions/MPISupportMissing.h
+++ src/TNL/Exceptions/MPISupportMissing.h
@@ -1,14 +1,12 @@
 /***************************************************************************
                           MPISupportMissing.h  -  description
                              -------------------
-    begin                : Jun 11, 2017
+    begin                : Jun 11, 2018
     copyright            : (C) 2018 by Tomas Oberhuber et al.
     email                : tomas.oberhuber@fjfi.cvut.cz
  ***************************************************************************/
 
 /* See Copyright Notice in tnl/Copyright */
-
-// Implemented by: Jakub Klinkovsky
 
 #pragma once
 
