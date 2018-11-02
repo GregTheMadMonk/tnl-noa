87a1bc567fc51c632cc2be6ca859e8b4961e1312
Lukas Cejka <lukas.ostatek@gmail.com>
2018-11-02 13:47:29 +0100

    In SparseMatrixCopyTest.h: 	Renamed different tested and copied matrices in the testCopyAssignment and testConversion functions.

--- src/UnitTests/Matrices/SparseMatrixCopyTest.h
+++ src/UnitTests/Matrices/SparseMatrixCopyTest.h
@@ -340,57 +340,57 @@ void checkTriDiagMatrix( Matrix& m )
 template< typename Matrix1, typename Matrix2 >
 void testCopyAssignment()
 {
-//   Matrix1 m1;
-//   setupTriDiagMatrix( m1 );
-//   checkTriDiagMatrix( m1 );
+   Matrix1 triDiag1;
+   setupTriDiagMatrix( triDiag1 );
+   checkTriDiagMatrix( triDiag1 );
    
-//   Matrix1 m11;
-//   setupAntiTriDiagMatrix( m11 );
-//   checkAntiTriDiagMatrix( m11 );
+   Matrix1 antiTriDiag1;
+   setupAntiTriDiagMatrix( antiTriDiag1 );
+   checkAntiTriDiagMatrix( antiTriDiag1 );
    
-   Matrix1 m111;
-   setupUnevenRowSizeMatrix( m111 );
-   checkUnevenRowSizeMatrix( m111 );
+   Matrix1 unevenRowSize1;
+   setupUnevenRowSizeMatrix( unevenRowSize1 );
+   checkUnevenRowSizeMatrix( unevenRowSize1 );
 
-//   Matrix2 m2;
-//   m2 = m1;
-//   checkTriDiagMatrix( m2 );
+   Matrix2 triDiag2;
+   triDiag2 = triDiag1;
+   checkTriDiagMatrix( triDiag2 );
    
-//   Matrix2 m22;
-//   m22 = m11;
-//   checkAntiTriDiagMatrix( m22 );
+   Matrix2 antiTriDiag2;
+   antiTriDiag2 = antiTriDiag1;
+   checkAntiTriDiagMatrix( antiTriDiag2 );
    
-   Matrix2 m222;
-   m222 = m111;
-   checkUnevenRowSizeMatrix( m222 );
+   Matrix2 unevenRowSize2;
+   unevenRowSize2 = unevenRowSize1;
+   checkUnevenRowSizeMatrix( unevenRowSize2 );
 }
 
 template< typename Matrix1, typename Matrix2 >
 void testConversion()
 {
-//   Matrix1 m1;
-//   setupTriDiagMatrix( m1 );
-//   checkTriDiagMatrix( m1 );
+   Matrix1 triDiag1;
+   setupTriDiagMatrix( triDiag1 );
+   checkTriDiagMatrix( triDiag1 );
    
-//   Matrix1 m11;
-//   setupAntiTriDiagMatrix( m11 );
-//   checkAntiTriDiagMatrix( m11 );
+   Matrix1 antiTriDiag1;
+   setupAntiTriDiagMatrix( antiTriDiag1 );
+   checkAntiTriDiagMatrix( antiTriDiag1 );
    
-   Matrix1 m111;
-   setupUnevenRowSizeMatrix( m111 );
-   checkUnevenRowSizeMatrix( m111 );
+   Matrix1 unevenRowSize1;
+   setupUnevenRowSizeMatrix( unevenRowSize1 );
+   checkUnevenRowSizeMatrix( unevenRowSize1 );
 
-//   Matrix2 m2;
-//   TNL::Matrices::copySparseMatrix( m2, m1 );
-//   checkTriDiagMatrix( m2 );
+   Matrix2 triDiag2;
+   TNL::Matrices::copySparseMatrix( triDiag2, triDiag1 );
+   checkTriDiagMatrix( triDiag2 );
    
-//   Matrix2 m22;
-//   TNL::Matrices::copySparseMatrix( m22, m11 );
-//   checkAntiTriDiagMatrix( m22 );
+   Matrix2 antiTriDiag2;
+   TNL::Matrices::copySparseMatrix( antiTriDiag2, antiTriDiag1 );
+   checkAntiTriDiagMatrix( antiTriDiag2 );
    
-   Matrix2 m222;
-   TNL::Matrices::copySparseMatrix( m222, m111 );
-   checkUnevenRowSizeMatrix( m222 );
+   Matrix2 unevenRowSize2;
+   TNL::Matrices::copySparseMatrix( unevenRowSize2, unevenRowSize1 );
+   checkUnevenRowSizeMatrix( unevenRowSize2 );
 }
 
 
