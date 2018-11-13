63b5f538473e6d618168d7b26e8c6a6e769ff7fd
Nina Dzugasova <dzugasova.nina@gmail.com>
2018-11-13 09:50:04 +0100

    Added unit tests for Timer.

---
+++ src/UnitTests/TimerTest.cpp
@@ -0,0 +1,45 @@
+/***************************************************************************
+                          FileNameTest.cpp  -  description
+                             -------------------
+    begin                : Oct 17, 2018
+    copyright            : (C) 2018 by Tomas Oberhuber
+    email                : tomas.oberhuber@fjfi.cvut.cz
+ ***************************************************************************/
+
+/* See Copyright Notice in tnl/Copyright */
+
+// Implemented by Nina Dzugasova
+
+#ifdef HAVE_GTEST 
+#include <gtest/gtest.h>
+#endif
+
+#include <TNL/Timer.h>
+
+using namespace TNL;
+
+#ifdef HAVE_GTEST 
+TEST( TimerTest, Constructor )
+{
+    Timer time;
+    time.reset();
+    EXPECT_EQ(time.getRealTime(),0);
+    time.start();
+    time.stop();
+    EXPECT_NE(time.getRealTime(),0);
+}
+#endif
+
+
+#include "GtestMissingError.h"
+int main( int argc, char* argv[] )
+{
+#ifdef HAVE_GTEST
+   ::testing::InitGoogleTest( &argc, argv );
+   return RUN_ALL_TESTS();
+#else
+   throw GtestMissingError();
+#endif
+}
+
+
