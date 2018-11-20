81a69d823ddd2a3540604d5a8c0348361bef5ce5
Nina Dzugasova <dzugasova.nina@gmail.com>
2018-11-20 15:52:03 +0100

    Added adjustments in documentation of Timer and File.

--- src/Examples/TimerExample.cpp
+++ src/Examples/TimerExample.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <TNL/Timer.h>
+#include <unistd.h>
 
 using namespace TNL;
 using namespace std;
        
 int main()
 {
+    unsigned int microseconds = 0.5;
     Timer time;
     time.start();
+    usleep(microseconds);
     time.stop();
     time.getRealTime();
     time.reset();
