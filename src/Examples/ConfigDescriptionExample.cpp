0bafd8d939245098c97ed10674331819307b96f2
Nina Dzugasova <dzugasova.nina@gmail.com>
2018-12-05 17:07:43 +0100

    Added ListExample and ConfigDescriptionExample.

--- src/Examples/ConfigDescriptionExample.cpp
+++ src/Examples/ConfigDescriptionExample.cpp
@@ -7,5 +7,8 @@ using namespace std;
 int main()
 {
     ConfigDescription confd;
-    
+    confd.template addEntry< String >("--new-entry","Specific description.");
+    confd.template addEntryEnum< String >("option1");
+    confd.template addEntryEnum< String >("option2");
+    confd.addDelimiter("-----------------------------");
 }
