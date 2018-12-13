9cfd6a53c1c1cc121bfaf21f8573e4a4b77af195
Nina Dzugasova <dzugasova.nina@gmail.com>
2018-12-13 14:43:51 +0100

    Corrected FileExample.

--- src/Examples/FileExample.cpp
+++ src/Examples/FileExample.cpp
@@ -7,18 +7,18 @@ using namespace std;
        
 int main()
 {
-    /*File file;
+    File file;
 
     file.open( String("new-file.tnl"), IOMode::write );
     String title("Header");
-    file.write( title );
+    file.write( &title );
     file.close();
 
     file.open( String("new-file.tnl"), IOMode::read );
     String title2;
-    file.read( title2, 4);
+    file.read( &title2, 4);
     file.close();
 
-    cout << "title2:" << title2 <<endl;*/
+    cout << "title2:" << title2 <<endl;
 }
 
