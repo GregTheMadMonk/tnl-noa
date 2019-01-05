cd5d21ac15929722ff459d89430b69b4e39bd9d7
Tomáš Oberhuber <oberhuber.tomas@gmail.com>
2019-01-05 20:17:09 +0100

    Implemented SimpleCell traverser benchmark test.

--- src/Benchmarks/Traversers/GridTraverserBenchmarkHelper.h
+++ src/Benchmarks/Traversers/GridTraverserBenchmarkHelper.h
@@ -37,18 +37,19 @@ _GridTraverser1D(
    typedef Real RealType;
    typedef Index IndexType;
    typedef Meshes::Grid< 1, Real, Devices::Cuda, Index > GridType;
-   typename GridType::CoordinatesType coordinates;
+   //typename GridType::CoordinatesType coordinates;
  
-   GridEntity entity;//( *grid, );
-   //entity.getCoordinates().x() = begin.x() + ( gridIdx * Devices::Cuda::getMaxGridSize() + blockIdx.x ) * blockDim.x + threadIdx.x;
-   coordinates.x() = begin.x() + ( gridIdx * Devices::Cuda::getMaxGridSize() + blockIdx.x ) * blockDim.x + threadIdx.x;
-   if( coordinates <= end )
+   GridEntity entity( *grid );
+   entity.getCoordinates().x() = begin.x() + ( gridIdx * Devices::Cuda::getMaxGridSize() + blockIdx.x ) * blockDim.x + threadIdx.x;
+   //coordinates.x() = begin.x() + ( gridIdx * Devices::Cuda::getMaxGridSize() + blockIdx.x ) * blockDim.x + threadIdx.x;
+   if( entity.getCoordinates() <= end )
    {
-      //entity.refresh();
+      entity.refresh();
       //( userData.u->getData() )[ entity.getIndex( coordinates ) ] += ( RealType ) 1.0;
       //( userData.u->getData() )[ coordinates.x() ] += ( RealType ) 1.0;
-      userData.data[ coordinates.x() ] += ( RealType ) 1.0;
-      //( *userData.u )( entity ) += ( RealType ) 1.0;
+      //userData.data[ entity.getIndex() ] += ( RealType ) 1.0;
+      //userData.u->getData()[ entity.getIndex() ] += ( RealType ) 1.0;
+      ( *userData.u )( entity ) += ( RealType ) 1.0;
       //EntitiesProcessor::processEntity( entity.getMesh(), userData, entity );
    }
 }
@@ -80,22 +81,25 @@ class GridTraverserBenchmarkHelper< Grid, Devices::Host >
                                      UserDataType& userData,
                                      std::size_t size )
       {
-         /*Meshes::GridTraverser< Grid >::template processEntities< Cell, WriteOneEntitiesProcessorType, WriteOneTraverserUserDataType, false >(
+         /*Meshes::GridTraverser< Grid >::template processEntities< CellType, AddOneEntitiesProcessorType, UserDataType, false >(
            grid,
            CoordinatesType( 0 ),
            grid->getDimensions() - CoordinatesType( 1 ),
-           userData );*/
-
+           userData );
+          */
+         
          const CoordinatesType begin( 0 );
          const CoordinatesType end = CoordinatesType( size ) - CoordinatesType( 1 );
          //MeshFunction* _u = &u.template modifyData< Device >();
-         /*SimpleCellType entity( *grid );
+         SimpleCellType entity( *grid );
          for( IndexType x = begin.x(); x <= end.x(); x ++ )
          {
             entity.getCoordinates().x() = x;
             entity.refresh();
-            AddOneEntitiesProcessorType::processEntity( entity.getMesh(), userData, entity );
-         }*/
+            //userData.u->getData()[ entity.getIndex() ] += ( RealType ) 1.0;
+            ( *userData.u )( entity ) += ( RealType ) 1.0;
+            //AddOneEntitiesProcessorType::processEntity( entity.getMesh(), userData, entity );
+         }
 
       }
 };
