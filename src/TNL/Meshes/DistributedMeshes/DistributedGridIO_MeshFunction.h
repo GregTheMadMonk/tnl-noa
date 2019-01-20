f7d28f7ded0297c2d005fe10b4c15b17d25de1f9
Tomáš Oberhuber <oberhuber.tomas@gmail.com>
2019-01-20 14:37:20 +0100

    Fixing DitributedGridIO to work with general mesh functions.

--- src/TNL/Meshes/DistributedMeshes/DistributedGridIO_MeshFunction.h
+++ src/TNL/Meshes/DistributedMeshes/DistributedGridIO_MeshFunction.h
@@ -504,7 +504,6 @@ class DistributedGridIO< MeshFunction,
 
       static bool load(const String& fileName,MeshFunctionType &meshFunction) 
       {
-#ifdef HAVE_MPI
          if(Communicators::MpiCommunicator::IsInitialized())//i.e. - isUsed
          {
             typename MeshFunctionType::RealType* data = meshFunction.getData().getData();
