41348dac910dad6d84ba9ebd111b0f05b038d942
Tomas Oberhuber <tomas.oberhuber@fjfi.cvut.cz>
2020-03-03 10:59:17 +0100

    Deleted unused dense matrix device dependent code.

--- src/TNL/Matrices/DenseMatrixView.hpp
+++ src/TNL/Matrices/DenseMatrixView.hpp
@@ -699,51 +699,5 @@ Index DenseMatrixView< Real, Device, Index, RowMajorOrder >::getElementIndex( co
    return this->segments.getGlobalIndex( row, column );
 }
 
-/*template<>
-class DenseDeviceDependentCode< Devices::Host >
-{
-   public:
-
-      typedef Devices::Host Device;
-
-      template< typename Real,
-                typename Index,
-                bool RowMajorOrder,
-                typename RealAllocator,
-                typename InVector,
-                typename OutVector >
-      static void vectorProduct( const DenseMatrixView< Real, Device, Index, RowMajorOrder >& matrix,
-                                 const InVector& inVector,
-                                 OutVector& outVector )
-      {
-#ifdef HAVE_OPENMP
-#pragma omp parallel for if( Devices::Host::isOMPEnabled() )
-#endif
-         for( Index row = 0; row < matrix.getRows(); row ++ )
-            outVector[ row ] = matrix.rowVectorProduct( row, inVector );
-      }
-};
-
-template<>
-class DenseDeviceDependentCode< Devices::Cuda >
-{
-   public:
-
-      typedef Devices::Cuda Device;
-
-      template< typename Real,
-                typename Index,
-                bool RowMajorOrder,
-                typename RealAllocator,
-                typename InVector,
-                typename OutVector >
-      static void vectorProduct( const DenseMatrixView< Real, Device, Index, RowMajorOrder >& matrix,
-                                 const InVector& inVector,
-                                 OutVector& outVector )
-      {
-         MatrixVectorProductCuda( matrix, inVector, outVector );
-      }
-};*/
-
 } // namespace Matrices
 } // namespace TNL
