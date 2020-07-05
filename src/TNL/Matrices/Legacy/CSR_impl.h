91ed0ebe057e1cba5431d5950a4697c4c8f79ded
Illia Kolesnik <kolesill@fit.cvut.cz>
2020-07-06 01:00:14 +0200

    Uncommented kernels

--- src/TNL/Matrices/Legacy/CSR_impl.h
+++ src/TNL/Matrices/Legacy/CSR_impl.h
@@ -1627,43 +1627,43 @@ class CSRDeviceDependentCode< Devices::Cuda >
                                                               inVector.getData(),
                                                               outVector.getData() );
 #else
-         // switch(KernelType)
-         // {
-         //    case CSRScalar:
-         //       SpMVCSRScalarPrepare<Real, Index, 32>(
-         //          inVector.getData(),
-         //          outVector.getData(),
-         //          matrix.getRowPointers().getData(),
-         //          matrix.getColumnIndexes().getData(),
-         //          matrix.getValues().getData(),
-         //          matrix.getRowPointers().getSize() - 1,
-         //          matrix.getColumns()
-         //       );
-         //       break;
-         //    case CSRVector:
-         //       SpMVCSRVectorPrepare<Real, Index, 32>(
-         //          inVector.getData(),
-         //          outVector.getData(),
-         //          matrix.getRowPointers().getData(),
-         //          matrix.getColumnIndexes().getData(),
-         //          matrix.getValues().getData(),
-         //          matrix.getRowPointers().getSize() - 1,
-         //          matrix.getColumns()
-         //       );
-         //       break;
-         //    case CSRLight:
-         //       SpMVCSRLightPrepare<Real, Index, 32>(
-         //          inVector.getData(),
-         //          outVector.getData(),
-         //          matrix.getRowPointers().getData(),
-         //          matrix.getColumnIndexes().getData(),
-         //          matrix.getValues().getData(),
-         //          matrix.getValues().getSize(),
-         //          matrix.getRowPointers().getSize() - 1,
-         //          matrix.getColumns()
-         //       );
-         //       break;
-         //    case CSRAdaptive:
+         switch(KernelType)
+         {
+            case CSRScalar:
+               SpMVCSRScalarPrepare<Real, Index, 32>(
+                  inVector.getData(),
+                  outVector.getData(),
+                  matrix.getRowPointers().getData(),
+                  matrix.getColumnIndexes().getData(),
+                  matrix.getValues().getData(),
+                  matrix.getRowPointers().getSize() - 1,
+                  matrix.getColumns()
+               );
+               break;
+            case CSRVector:
+               SpMVCSRVectorPrepare<Real, Index, 32>(
+                  inVector.getData(),
+                  outVector.getData(),
+                  matrix.getRowPointers().getData(),
+                  matrix.getColumnIndexes().getData(),
+                  matrix.getValues().getData(),
+                  matrix.getRowPointers().getSize() - 1,
+                  matrix.getColumns()
+               );
+               break;
+            case CSRLight:
+               SpMVCSRLightPrepare<Real, Index, 32>(
+                  inVector.getData(),
+                  outVector.getData(),
+                  matrix.getRowPointers().getData(),
+                  matrix.getColumnIndexes().getData(),
+                  matrix.getValues().getData(),
+                  matrix.getValues().getSize(),
+                  matrix.getRowPointers().getSize() - 1,
+                  matrix.getColumns()
+               );
+               break;
+            case CSRAdaptive:
                SpMVCSRAdaptivePrepare<Real, Index, Device, KernelType, 32>(
                   inVector.getData(),
                   outVector.getData(),
@@ -1675,32 +1675,32 @@ class CSRDeviceDependentCode< Devices::Cuda >
                   matrix.getRowPointers().getSize(),
                   matrix.getColumns()
                );
-            //    break;
-            // case CSRMultiVector:
-            //    SpMVCSRMultiVectorPrepare<Real, Index, 32>(
-            //       inVector.getData(),
-            //       outVector.getData(),
-            //       matrix.getRowPointers().getData(),
-            //       matrix.getColumnIndexes().getData(),
-            //       matrix.getValues().getData(),
-            //       matrix.getValues().getSize(),
-            //       matrix.getRowPointers().getSize() - 1,
-            //       matrix.getColumns()
-            //    );
-            //    break;
-            // case CSRLightWithoutAtomic:
-            //    SpMVCSRLightWithoutAtomicPrepare<Real, Index, 32>(
-            //       inVector.getData(),
-            //       outVector.getData(),
-            //       matrix.getRowPointers().getData(),
-            //       matrix.getColumnIndexes().getData(),
-            //       matrix.getValues().getData(),
-            //       matrix.getValues().getSize(),
-            //       matrix.getRowPointers().getSize() - 1,
-            //       matrix.getColumns()
-            //    );
-            //    break;
-         // }
+               break;
+            case CSRMultiVector:
+               SpMVCSRMultiVectorPrepare<Real, Index, 32>(
+                  inVector.getData(),
+                  outVector.getData(),
+                  matrix.getRowPointers().getData(),
+                  matrix.getColumnIndexes().getData(),
+                  matrix.getValues().getData(),
+                  matrix.getValues().getSize(),
+                  matrix.getRowPointers().getSize() - 1,
+                  matrix.getColumns()
+               );
+               break;
+            case CSRLightWithoutAtomic:
+               SpMVCSRLightWithoutAtomicPrepare<Real, Index, 32>(
+                  inVector.getData(),
+                  outVector.getData(),
+                  matrix.getRowPointers().getData(),
+                  matrix.getColumnIndexes().getData(),
+                  matrix.getValues().getData(),
+                  matrix.getValues().getSize(),
+                  matrix.getRowPointers().getSize() - 1,
+                  matrix.getColumns()
+               );
+               break;
+         }
 #endif /* HAVE_CUDA */
 #endif
       }
