c1b8c44fd60cf3933d88cee0e94ac3c17273ea92
Tomáš Oberhuber <oberhuber.tomas@gmail.com>
2021-01-22 16:21:40 +0100

    Added unit tests for CSR Vector sparse matrix.

---
+++ src/UnitTests/Matrices/SparseMatrixTest_CSRVector.cpp
@@ -0,0 +1,11 @@
+/***************************************************************************
+                          SparseMatrixTest_CSRVector.cpp -  description
+                             -------------------
+    begin                : Jan 22, 2021
+    copyright            : (C) 2021 by Tomas Oberhuber et al.
+    email                : tomas.oberhuber@fjfi.cvut.cz
+ ***************************************************************************/
+
+/* See Copyright Notice in tnl/Copyright */
+
+#include "SparseMatrixTest_CSRScalar.h"
