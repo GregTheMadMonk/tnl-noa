2654ee7bfb44dc7e3bac791953832241f4481429
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2021-02-15 00:23:29 +0100

    sorting pow2 arrays

--- GPUSort/bitonicGPU/bitonicSort.h
+++ GPUSort/bitonicGPU/bitonicSort.h
@@ -1,15 +1,18 @@
 #include <TNL/Containers/Array.h>
+#include <iostream>
 
 using namespace TNL;
 using namespace TNL::Containers;
 
 typedef Devices::Cuda Device;
 
+#define deb(x) std::cout << #x << " = " << x << std::endl;
+
 //---------------------------------------------
 
 __host__ __device__ int closestPow2(int x)
 {
-    if(x ==0)
+    if (x == 0)
         return 0;
 
     int ret = 1;
@@ -22,8 +25,8 @@ __host__ __device__ int closestPow2(int x)
 //---------------------------------------------
 
 __global__ void bitonicMergeStep(ArrayView<int, Device> arr,
-                            int begin, int end, bool sortAscending,
-                            int monotonicSeqLen, int len, int partsInSeq)
+                                 int begin, int end, bool sortAscending,
+                                 int monotonicSeqLen, int len, int partsInSeq)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -49,13 +52,12 @@ __global__ void bitonicMergeStep(ArrayView<int, Device> arr,
 }
 
 __global__ void bitonicMergeSharedMemory(ArrayView<int, Device> arr,
-                            int begin, int end, bool sortAscending,
-                            int monotonicSeqLen, int len, int partsInSeq)
+                                         int begin, int end, bool sortAscending,
+                                         int monotonicSeqLen, int len, int partsInSeq)
 {
     extern __shared__ int sharedMem[];
 
-
-    int s = begin + blockIdx.x * (2*blockDim.x);
+    int s = begin + blockIdx.x * (2 * blockDim.x) + threadIdx.x;
     int e = s + blockDim.x;
     //copy from globalMem into sharedMem
     {
@@ -63,16 +65,16 @@ __global__ void bitonicMergeSharedMemory(ArrayView<int, Device> arr,
         sharedMem[threadIdx.x + blockDim.x] = arr[e];
         __syncthreads();
     }
-    
+
     //------------------------------------------
     //bitonic activity
-    {    
+    {
         //calculate the direction of swapping
         int i = blockIdx.x * blockDim.x + threadIdx.x;
         int part = i / (len / 2);
         int monotonicSeqIdx = part / partsInSeq;
-        bool ascending = (monotonicSeqIdx % 2) == 0 ? !sortAscending : sortAscending;
 
+        bool ascending = (monotonicSeqIdx % 2) == 0 ? !sortAscending : sortAscending;
         //special case for parts with no "partner"
         if ((monotonicSeqIdx + 1) * monotonicSeqLen >= end)
             ascending = sortAscending;
@@ -83,10 +85,8 @@ __global__ void bitonicMergeSharedMemory(ArrayView<int, Device> arr,
         {
             __syncthreads();
 
-            int i = threadIdx.x;
-
-            int part = i/(len/2);
-            int s = part * len + (i% (len / 2));
+            int part = threadIdx.x / (len / 2);
+            int s = part * len + (threadIdx.x % (len / 2));
             int e = s + len / 2;
 
             //swap
@@ -115,15 +115,27 @@ void bitonicSort(ArrayView<int, Device> arr, int begin, int end, bool sortAscend
     int arrSize = end - begin;
     int paddedSize = closestPow2(arrSize);
 
-    int threadPerBlock = 256;
-    int blocks = arrSize/threadPerBlock + (arrSize%threadPerBlock == 0? 0 : 1);
+    int threadsNeeded = arrSize / 2;
+
+    const int maxThreadsPerBlock = 256;
+    int threadPerBlock = min(maxThreadsPerBlock, threadsNeeded);
+    int blocks = threadsNeeded / threadPerBlock + (threadsNeeded % threadPerBlock == 0 ? 0 : 1);
+
+    const int sharedMemSize = threadPerBlock * 2 * sizeof(int);
 
     for (int monotonicSeqLen = 2; monotonicSeqLen <= paddedSize; monotonicSeqLen *= 2)
     {
         for (int len = monotonicSeqLen, partsInSeq = 1; len > 1; len /= 2, partsInSeq *= 2)
         {
-            bitonicMergeStep<<<blocks, threadPerBlock>>>(arr, begin, end, sortAscending, 
-                                                        monotonicSeqLen, len, partsInSeq);
+            /*
+            bitonicMergeStep<<<blocks, threadPerBlock>>>(arr, begin, end, sortAscending,
+                                                         monotonicSeqLen, len, partsInSeq);
+            */
+            bitonicMergeSharedMemory<<<blocks, threadPerBlock, sharedMemSize>>>(arr, begin, end, sortAscending,
+                                                                                monotonicSeqLen, len, partsInSeq);
+
+            cudaDeviceSynchronize();
+            break;
         }
     }
 }
