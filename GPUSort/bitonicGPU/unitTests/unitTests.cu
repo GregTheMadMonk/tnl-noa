adf71f08e27f0e4f27447765e5f42cb14c80de46
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2021-02-15 22:22:08 +0100

    big array tests and unify permutations

--- GPUSort/bitonicGPU/unitTests/unitTests.cu
+++ GPUSort/bitonicGPU/unitTests/unitTests.cu
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
+#include <random>
 
 #include <TNL/Containers/Array.h>
 
@@ -11,8 +12,8 @@
 
 bool is_sorted(TNL::Containers::ArrayView<int, TNL::Devices::Cuda> arr)
 {
-    for(int i = 1; i < arr.getSize(); i++)
-        if(arr.getElement(i-1) > arr.getElement(i))
+    for (int i = 1; i < arr.getSize(); i++)
+        if (arr.getElement(i - 1) > arr.getElement(i))
             return false;
 
     return true;
@@ -20,27 +21,30 @@ bool is_sorted(TNL::Containers::ArrayView<int, TNL::Devices::Cuda> arr)
 
 //----------------------------------------------------------------------------------
 
-TEST(sortPow2, allPermutationSize4)
+TEST(permutations, allPermutationSize_3_to_7)
 {
-    int size = 4;
-    std::vector<int> orig(size);
-    std::iota(orig.begin(), orig.end(), 0);
-
-    while (std::next_permutation(orig.begin(), orig.end()))
+    for(int i = 3; i<=7; i++ )
     {
-        TNL::Containers::Array<int, Device> cudaArr(orig);
-        auto view = cudaArr.getView();
+        int size = i;
+        std::vector<int> orig(size);
+        std::iota(orig.begin(), orig.end(), 0);
 
-        bitonicSort(view);
+        while (std::next_permutation(orig.begin(), orig.end()))
+        {
+            TNL::Containers::Array<int, Device> cudaArr(orig);
+            auto view = cudaArr.getView();
 
-        ASSERT_TRUE(is_sorted(view));
+            bitonicSort(view);
+
+            ASSERT_TRUE(is_sorted(view)) << "failed " << i << std::endl;
+        } 
     }
 }
 
-TEST(sortPow2, somePermutationSize8)
+TEST(permutations, somePermutationSize8)
 {
     int size = 8;
-    const int stride = 5;
+    const int stride = 23;
     int i = 0;
 
     std::vector<int> orig(size);
@@ -48,7 +52,7 @@ TEST(sortPow2, somePermutationSize8)
 
     while (std::next_permutation(orig.begin(), orig.end()))
     {
-        if((i++)%stride != 0)
+        if ((i++) % stride != 0)
             continue;
 
         TNL::Containers::Array<int, Device> cudaArr(orig);
@@ -56,53 +60,84 @@ TEST(sortPow2, somePermutationSize8)
 
         bitonicSort(view);
 
+        ASSERT_TRUE(is_sorted(view)) << "result " << view << std::endl;
+    }
+}
+
+TEST(permutations, somePermutationSize9)
+{
+    int size = 9;
+    const int stride = 227;
+    int i = 0;
+
+    std::vector<int> orig(size);
+    std::iota(orig.begin(), orig.end(), 0);
+
+    while (std::next_permutation(orig.begin(), orig.end()))
+    {
+        if ((i++) % stride != 0)
+            continue;
+
+        TNL::Containers::Array<int, Device> cudaArr(orig);
+        auto view = cudaArr.getView();
+
+        bitonicSort(view);
+
+        ASSERT_TRUE(is_sorted(view)) << "result " << view << std::endl;
+    }
+}
+
+TEST(selectedSize, size15)
+{
+    TNL::Containers::Array<int, Device> cudaArr{5, 9, 4, 8, 6, 1, 2, 3, 4, 8, 1, 6, 9, 4, 9};
+    auto view = cudaArr.getView();
+    ASSERT_EQ(15, view.getSize());
+    bitonicSort(view);
+    ASSERT_TRUE(is_sorted(view)) << "result " << view << std::endl;
+}
+
+TEST(multiblock, 32768_decreasingNegative)
+{
+    TNL::Containers::Array<int, Device> cudaArr(1 << 15);
+    for (int i = 0; i < cudaArr.getSize(); i++)
+        cudaArr.setElement(i, -i);
+
+    auto view = cudaArr.getView();
+    bitonicSort(view);
+    ASSERT_TRUE(is_sorted(view)) << "result " << view << std::endl;
+}
+
+TEST(randomGenerated, smallArray_randomVal)
+{
+    for(int i = 0; i < 100; i++)
+    {
+        TNL::Containers::Array<int, Device> cudaArr(std::rand()%(1<<10));
+        for (int j = 0; j < cudaArr.getSize(); j++)
+            cudaArr.setElement(j, std::rand());
+
+        auto view = cudaArr.getView();
+        bitonicSort(view);
         ASSERT_TRUE(is_sorted(view));
     }
 }
 
-
-TEST(selectedSize, size5)
+TEST(randomGenerated, bigArray_all0)
 {
-    TNL::Containers::Array<int, Device> cudaArr{8, 1, 45, 9, -5};
-    auto view = cudaArr.getView();
-    ASSERT_EQ(5, view.getSize());
-    bitonicSort(view);
-    ASSERT_TRUE(is_sorted(view));
+    for(int i = 0; i < 50; i++)
+    {
+        int size = (1<<20) + (std::rand()% (1<<19));
+
+        TNL::Containers::Array<int, Device> cudaArr(size);
+
+        auto view = cudaArr.getView();
+        bitonicSort(view);
+        ASSERT_TRUE(true);
+    }
 }
-
-TEST(selectedSize, size6)
-{
-    TNL::Containers::Array<int, Device> cudaArr{5, 9, 4, 3, 4, 0};
-    auto view = cudaArr.getView();
-    ASSERT_EQ(6, view.getSize());
-    bitonicSort(view);
-    ASSERT_TRUE(is_sorted(view));
-}
-
-
-TEST(selectedSize, size7)
-{
-    TNL::Containers::Array<int, Device> cudaArr{5, 8, 1, 6, 9, 7, 1};
-    auto view = cudaArr.getView();
-    ASSERT_EQ(7, view.getSize());
-    bitonicSort(view);
-    ASSERT_TRUE(is_sorted(view));
-}
-
-TEST(selectedSize, size9)
-{
-    TNL::Containers::Array<int, Device> cudaArr{5, 8, 1, 6, 9, 7, 1, 6, 0};
-    auto view = cudaArr.getView();
-    ASSERT_EQ(9, view.getSize());
-    bitonicSort(view);
-    ASSERT_TRUE(is_sorted(view));
-}
-
-
 
 //----------------------------------------------------------------------------------
 
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
 
