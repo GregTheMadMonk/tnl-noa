8319727554b1899e37ab0f6b412dbb04ebe0adc5
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2021-03-03 18:42:48 +0100

    calculating amount of blocks needed

--- GPUSort/quicksort/quicksort.cuh
+++ GPUSort/quicksort/quicksort.cuh
@@ -2,6 +2,34 @@
 
 #include <TNL/Containers/Array.h>
 
+int partition(TNL::Containers::ArrayView<int, TNL::Devices::Cuda> arr, int begin, int end, int pivotIdx)
+{
+    int size = end - begin;
+    const int threadsPerBlock = 512, maxBlocks = 1<<14; //16k
+    int elemPerBlock, blocks;
+    
+    int setsNeeded = size/threadsPerBlock + (size % threadsPerBlock != 0);
+    if(setsNeeded <= blocks)
+    {
+        blocks = setsNeeded;
+        elemPerBlock = threadsPerBlock;
+    }
+    else
+    {
+        int setsPerBlock = setsNeeded/blocks + 1; //+1 to spread out task of the last block
+        elemPerBlock *= setsPerBlock;
+        blocks = size / elemPerBlock + (size % elemPerBlock != 0);
+    }
+
+    //------------------------------------
+
+
+
+}
+
+//-----------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------
+
 void quicksort(TNL::Containers::ArrayView<int, TNL::Devices::Cuda> arr, int begin, int end)
 {
     if(begin >= end) return;
