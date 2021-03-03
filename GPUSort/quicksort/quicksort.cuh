0686d9b43698729c1dc9d26dcc33765cd0ced36d
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2021-03-03 18:40:25 +0100

    rewrite

--- GPUSort/quicksort/quicksort.cuh
+++ GPUSort/quicksort/quicksort.cuh
@@ -2,38 +2,18 @@
 
 #include <TNL/Containers/Array.h>
 
-__global__ void quicksortCuda(TNL::Containers::ArrayView<int, TNL::Devices::Cuda> arr, int begin, int end)
+void quicksort(TNL::Containers::ArrayView<int, TNL::Devices::Cuda> arr, int begin, int end)
 {
-    if(begin >= end)
-        return;
+    if(begin >= end) return;
 
-    int pivotIdx = end - 1;
-    int pivot = arr[pivotIdx];
+    int newPivotPos = partition(arr, begin, end, end-1);
+    quicksort(arr, begin, newPivotPos);
+    quicksort(arr, newPivotPos + 1, end);
 
-    int midPoint = begin; //[begin ; midPoint) contain elems smaller than pivot
-
-    //partition the array except for last elem (the pivot itself) 
-    for(int i = begin; i + 1< end; i++)
-    {
-        if(arr[i] < pivot)
-        {
-            TNL::swap(arr[i], arr[midPoint]);
-            midPoint++; //increase boundary
-        }
-    }
-
-    //put pivot onto its correct position, now [begin, midpoint] is sorted
-    TNL::swap(arr[midPoint], arr[pivotIdx]);
-
-    //sorts all elems before midPoint(which is pivot now)
-    quicksortCuda<<<1, 1>>>(arr, begin, midPoint);
-
-    //sorts all elems after(bigger than) midPoint
-    quicksortCuda<<<1, 1>>>(arr, midPoint+1, end);
-
+    cudaDeviceSynchronize();
 }
 
 void quicksort(TNL::Containers::ArrayView<int, TNL::Devices::Cuda> arr)
 {
-    quicksortCuda<<<1, 1>>>(arr, 0, arr.getSize());
+    quicksort(arr, 0, arr.getSize());
 }
