743873260a1125f3076f579441058f0786fac580
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2021-03-06 23:56:46 +0100

    quicksort by depth in class

--- GPUSort/quicksort/quicksort.cuh
+++ GPUSort/quicksort/quicksort.cuh
@@ -2,6 +2,8 @@
 
 #include <TNL/Containers/Array.h>
 #include "reduction.cuh"
+#include "task.h"
+#include <utility>
 
 #define deb(x) std::cout << #x << " = " << x << std::endl;
 
@@ -124,6 +126,120 @@ void quicksort(CudaArrayView arr, int begin, int end)
 
     cudaDeviceSynchronize();
 }
+
+//-----------------------------------------------------------
+class QUICKSORT
+{
+    static const int threadsPerBlock = 512, maxBlocks = 1 << 14; //16k
+    const int maxTasks = 1<<20;
+
+    CudaArrayView arr;
+    int begin, end;
+    int size;
+    TNL::Containers::Array<int, TNL::Devices::Cuda> aux;
+
+    TNL::Containers::Array<TASK, TNL::Devices::Host> host_tasks;
+    TNL::Containers::Array<TASK, TNL::Devices::Cuda> cuda_tasks;
+    TNL::Containers::Array<TASK, TNL::Devices::Cuda> newTasks;
+    int tasksAmount;
+
+    TNL::Containers::Array<int, TNL::Devices::Host> host_blockToTaskMapping;
+    TNL::Containers::Array<int, TNL::Devices::Cuda> cuda_blockToTaskMapping;
+
+    //--------------------------------------------------------------------------------------
+
+    int getSetsNeeded()
+    {
+        auto view = host_tasks.getView();
+        auto fetch = [=] __cuda_callable__ (int i) {
+            auto task = view.getElement(i);
+            int size = task.arrEnd - task.arrBegin;
+            return size / threadsPerBlock + (size % threadsPerBlock != 0); 
+        };
+        auto reduction = [] __cuda_callable__(int a, int b) {return a + b;};
+        return TNL::Algorithms::Reduction<TNL::Devices::Host>::reduce(0, tasksAmount, reduction, fetch, 0);
+    }
+    
+    std::pair<int, int> calcConfig()
+    {
+        int setsNeeded = getSetsNeeded();
+
+        if(setsNeeded <= maxBlocks)
+            return {setsNeeded, threadsPerBlock};
+
+        int setsPerBlock = setsNeeded / maxBlocks + 1; //+1 to spread out task of the last block
+        int elemPerBlock = setsPerBlock * threadsPerBlock;
+        int blocks = size / elemPerBlock + (size % elemPerBlock != 0);
+        return {blocks, elemPerBlock};
+    }
+
+    int initTasks(std::pair<int, int> blocks_elemPerBlock)
+    {
+        int elemPerBlock = blocks_elemPerBlock.second;
+        auto host_tasksView = host_tasks.getView();
+        int blockToTaskMapping_Cnt = 0;
+
+        for(int i = 0; i < tasksAmount; ++i)
+        {
+            TASK & task = host_tasks[i];
+            int size = task.arrEnd - task.arrBegin;
+            int blocksNeeded = size / elemPerBlock + (size % elemPerBlock != 0);
+
+            task.firstBlock = blockToTaskMapping_Cnt;
+            task.blockCount = blocksNeeded;
+
+            for(int set = 0; set < blocksNeeded; set++)
+                host_blockToTaskMapping[blockToTaskMapping_Cnt++] = i;
+        }
+
+        TNL::Algorithms::MultiDeviceMemoryOperations<TNL::Devices::Cuda, TNL::Devices::Host >::
+        copy(cuda_tasks.getData(), host_tasks.getData(), host_tasks.getSize());
+
+        TNL::Algorithms::MultiDeviceMemoryOperations<TNL::Devices::Cuda, TNL::Devices::Host >::
+        copy(cuda_blockToTaskMapping.getData(), host_blockToTaskMapping.getData(), host_blockToTaskMapping.getSize());
+
+        if(blockToTaskMapping_Cnt != blocks_elemPerBlock.first)
+        {
+            std::cerr << "blockToTaskMapping_Cnt != blocks_elemPerBlock" << std::endl;
+            class INVALID_CONFIG{};
+            throw INVALID_CONFIG();
+        }
+
+        return blockToTaskMapping_Cnt;
+    }
+
+public:
+    QUICKSORT(CudaArrayView arr, int begin, int end)
+        : arr(arr.getView()), begin(begin), end(end),
+        size(end - begin), aux(size),
+        host_tasks(maxTasks), cuda_tasks(maxTasks), newTasks(maxTasks),
+        host_blockToTaskMapping(maxBlocks), cuda_blockToTaskMapping(maxBlocks)
+    {
+        int pivotIdx = end - 1;
+        host_tasks[0] = TASK(begin, end, 0, size, pivotIdx);
+        tasksAmount = 1;
+    }
+
+    void sort()
+    {
+        while(tasksAmount > 0)
+        {
+            std::pair<int, int> blocks_elemPerBlock = calcConfig();
+            int blocksCnt = initTasks(blocks_elemPerBlock);
+            /*
+            partition(arr, aux.getView(),
+                cuda_tasks.getView(), cuda_blockToTaskMapping.getView()
+                newTasks.getView());
+            */
+           processTasks();
+        }
+
+        //2nd phase to finish
+    }
+
+};
+
+//-----------------------------------------------------------
 
 void quicksort(CudaArrayView arr)
 {
---
+++ GPUSort/quicksort/task.h
@@ -0,0 +1,18 @@
+#pragma once
+
+struct TASK
+{
+    int arrBegin, arrEnd;//start and end position of array to read from
+    int auxBeginIdx, auxEndIdx; //start and end position of still available memory to write into
+    int pivotPos; //input pivot pos and output 
+    int firstBlock, blockCount; //shared counter of blocks working together(how many are still working)
+
+    TASK(int srcBegin, int srcEnd, int destBegin, int destEnd, int pivotPos)
+        : arrBegin(srcBegin), arrEnd(srcEnd),
+        auxBeginIdx(destBegin), auxEndIdx(destEnd),
+        pivotPos(pivotPos),
+        firstBlock(-1), blockCount(-1)
+        {}
+    TASK() = default;
+
+};
