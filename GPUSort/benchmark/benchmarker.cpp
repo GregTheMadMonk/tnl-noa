741e3368a490dee92f14f4a34be11c7e3a6fdae7
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2021-03-29 20:52:15 +0200

    add different inputs and better interface for measuring

--- GPUSort/benchmark/benchmarker.cpp
+++ GPUSort/benchmark/benchmarker.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 
 #include "../src/util/timer.h"
@@ -26,15 +27,27 @@ using namespace TNL::Containers;
 
 static int notCorrectCounters = 0;
 
-const int lowPow = 13, highLow = 25;
-const int tries = 50;
+#ifndef LOW_POW
+    #define LOW_POW 10
+#endif
 
+#ifndef HIGH_POW
+    #define HIGH_POW 25
+#endif
+
+#ifndef TRIES
+    #define TRIES 20
+#endif
+
+double measure(const vector<int>&vec);
+
+#ifndef MY_OWN_MEASURE
 double measure(const vector<int>&vec)
 {
     vector<double> resAcc;
 
 
-    for(int i = 0; i < tries; i++)
+    for(int i = 0; i < TRIES; i++)
     {
     #ifdef HAVE_CUDA
         Array<int, Devices::Cuda> arr(vec);
@@ -62,6 +75,7 @@ double measure(const vector<int>&vec)
 
     return accumulate(resAcc.begin(), resAcc.end(), 0.0) / resAcc.size();
 }
+#endif
 
 double sorted(int size)
 {
@@ -72,6 +86,16 @@ double sorted(int size)
 }
 
 double random(int size)
+{
+    srand(size + 2021);
+
+    vector<int> vec(size);
+    generate(vec.begin(), vec.end(), [=](){return std::rand() % (2*size);});
+
+    return measure(vec);
+}
+
+double shuffle(int size)
 {
     srand(size);
 
@@ -98,8 +122,8 @@ double almostSorted(int size)
 double decreasing(int size)
 {
     vector<int> vec(size);
-    for(size_t i = 0; i < size; i++)
-        vec[i] = -i;
+    for(int i = 0; i < size; i++)
+        vec[i] = size - i;
                 
     return measure(vec);
 }
@@ -113,17 +137,101 @@ double zero_entropy(int size)
     return measure(vec);
 }
 
+double gaussian(int size)
+{
+	srand(size + 2000);
+
+    vector<int> vec(size);
+	for (int i = 0; i < size; ++i)
+    {
+		int value = 0;
+		for (int j = 0; j < 4; ++j) 
+			value += rand()%16384;
+
+		vec[i] = value /4;
+	}
+    return measure(vec);  
+}
+
+double bucket(int size)
+{
+	srand (size + 94215);
+    vector<int> vec(size);
+    
+	double tmp = ((double)size)*3000000; //(RAND_MAX)/p; --> ((double)N)*30000;
+	double tmp2 = sqrt(tmp);
+
+	int p= (size+tmp2-1)/tmp2;
+
+	const int VALUE = 8192/p; //(RAND_MAX)/p;
+
+	int i=0; int x=0;
+	//the array of size N is split into 'p' buckets
+	while(i < p)
+	{
+		for (int z = 0; z < p; ++z)
+			for (int j = 0; j < size/(p*p); ++j)
+			{
+				//every bucket has N/(p*p) items and the range is [min : VALUE-1 ]
+				int min = VALUE*z;
+
+				vec[x]= min + ( rand() %  (VALUE-1) ) ;
+				x++;
+			}
+		i++;
+	}
+
+    return measure(vec);
+}
+
+double staggared(int size)
+{
+	srand (size + 815618);
+    vector<int> vec(size);
+
+	int tmp=4096; //(RAND_MAX)/p; --> size=2048
+	int p= (size+tmp-1)/tmp;
+
+	const int VALUE = 16384/p; //(RAND_MAX)/p;
+
+	int i=1; int x=0;
+	//the array of size N is split into 'p' buckets
+	while(i <= p)
+	{
+		//every bucket has N/(p) items
+		for (int j = 0; j < size/(p); ++j)
+		{
+			int min;
+
+			if(i<=(p/2))
+				min = (2*i -1)*VALUE;
+
+			else
+				min = (2*i-p-1)*VALUE;
+
+			vec[x++]= min + ( rand() % (VALUE - 1) );
+		}
+		i++;
+	}
+
+    return measure(vec);
+}
+
 void start(ostream & out, string delim)
 {
     out << "size" << delim;
     out << "random" << delim;
+    out << "shuffle" << delim;
     out << "sorted" << delim;
     out << "almost" << delim;
     out << "decreas" << delim;
+    out << "gauss" << delim;
+    out << "bucket" << delim;
+    out << "stagger" << delim;
     out << "zero_entropy";
     out << endl;
     
-    for(int pow = lowPow; pow <= highLow; pow++)
+    for(int pow = LOW_POW; pow <= HIGH_POW; pow++)
     {
         int size =(1<< pow);
         vector<int> vec(size);
@@ -131,9 +239,13 @@ void start(ostream & out, string delim)
         out << "2^" << pow << delim;
         out << fixed << setprecision(3);
         out << random(size) << delim;
+        out << shuffle(size) << delim;
         out << sorted(size) << delim;
         out << almostSorted(size) << delim;
         out << decreasing(size) << delim;
+        out << gaussian(size) << delim;
+        out << bucket(size) << delim;
+        out << staggared(size) << delim;
         out << zero_entropy(size);
         out << endl;
     }
@@ -147,13 +259,12 @@ int main(int argc, char *argv[])
     }
     else
     {
-        ofstream out(argv[1]);
+        std::ofstream out(argv[1]);
         start(out, ",");
     }
-    
     if(notCorrectCounters > 0)
     {
-        std::cerr << notCorrectCounters << " tries were sorted incorrectly" << std::endl;
+        std::cerr << notCorrectCounters << "tries were sorted incorrectly" << std::endl;
     }
     return 0;
 }
