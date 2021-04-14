e3a4ae4950f97e5382c1528799bee08478254968
Jakub Klinkovský <klinkovsky@mmg.fjfi.cvut.cz>
2021-04-14 15:42:41 +0200

    MPI: fixed bug due to std::getenv returning a null pointer

--- src/TNL/MPI/selectGPU.h
+++ src/TNL/MPI/selectGPU.h
@@ -28,9 +28,14 @@ inline void selectGPU()
 
    const int local_rank = getRankOnNode();
    const int gpuNumber = local_rank % gpuCount;
+
+   // write debug output before calling cudaSetDevice
+   const char* cuda_visible_devices = std::getenv("CUDA_VISIBLE_DEVICES");
+   if( !cuda_visible_devices )
+      cuda_visible_devices = "";
    std::cout << "Rank " << GetRank() << ": rank on node is " << local_rank
              << ", using GPU id " << gpuNumber << " of " << gpuCount
-             << ", CUDA_VISIBLE_DEVICES=" << std::getenv("CUDA_VISIBLE_DEVICES")
+             << ", CUDA_VISIBLE_DEVICES=" << cuda_visible_devices
              << std::endl;
 
    cudaSetDevice(gpuNumber);
