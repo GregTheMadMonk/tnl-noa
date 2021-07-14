d6882717e20cb6e9b4687ffcc22dc23862ce58f6
Tomáš Oberhuber <oberhuber.tomas@gmail.com>
2021-07-14 20:56:12 +0200

    Added wrapper for STL sort on CPU.

---
+++ src/TNL/Algorithms/Sorting/STLSort.h
@@ -0,0 +1,40 @@
+/***************************************************************************
+                          STLSort.h  -  description
+                             -------------------
+    begin                : Jul 14, 2021
+    copyright            : (C) 2021 by Tomas Oberhuber et al.
+    email                : tomas.oberhuber@fjfi.cvut.cz
+ ***************************************************************************/
+
+/* See Copyright Notice in tnl/Copyright */
+
+// Implemented by: Tomas Oberhuber
+
+#pragma once
+
+#include <algorithm>
+
+namespace TNL {
+   namespace Algorithms {
+      namespace Sorting {
+
+struct STLSort
+{
+   template< typename Array >
+   void static sort( Array& array )
+   {
+      std::sort( array.getData(), array.getData() + array.getSize() );
+   }
+
+   template< typename Array, typename Compare >
+   void static sort( Array& array, const Compare& compare )
+   {
+      std::sort( array.getData(), array.getData() + array.getSize(), compare );
+   }
+};
+
+      } // namespace Sorting
+   } // namespace Algorithms
+} //namespace TNL
+
+
