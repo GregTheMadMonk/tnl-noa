566e0bba0d903ee8793f5d54f8dfef8d04a80343
Tomáš Oberhuber <oberhuber.tomas@gmail.com>
2021-12-22 16:27:04 +0100

    Writting documentation for TNL::Solvers namespace.

---
+++ src/TNL/Solvers/_NamespaceDoxy.h
@@ -0,0 +1,21 @@
+/***************************************************************************
+                          _NamespaceDoxy.h  -  description
+                             -------------------
+    begin                : Dec 21, 2021
+    copyright            : (C) 2021 by Tomas Oberhuber et al.
+    email                : tomas.oberhuber@fjfi.cvut.cz
+ ***************************************************************************/
+
+/* See Copyright Notice in tnl/Copyright */
+
+#pragma once
+
+namespace TNL {
+
+   /**
+    * \brief Namespace for solvers.
+    */
+   namespace Solvers {
+
+   } // namespace Solvers
+} // namespace TNL
