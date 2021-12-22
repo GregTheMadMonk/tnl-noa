43ff25126865843a637cfd03d5b86f5059a3202d
Tomáš Oberhuber <oberhuber.tomas@gmail.com>
2021-12-22 16:25:20 +0100

    Writtin documentation for TFQMR method.

--- src/TNL/Solvers/Linear/TFQMR.h
+++ src/TNL/Solvers/Linear/TFQMR.h
@@ -10,34 +10,72 @@
 
 #pragma once
 
-#include "LinearSolver.h"
+#include <TNL/Solvers/Linear/LinearSolver.h>
 
 namespace TNL {
-namespace Solvers {
-namespace Linear {
+   namespace Solvers {
+      namespace Linear {
 
+/**
+ * \brief Iterative solver of linear systems based on the Transpose-free quasi-minimal residual (TFQMR) method.
+ *
+ * See (Wikipedia)[https://second.wiki/wiki/algoritmo_tfqmr] for more details.
+ *
+ * \tparam Matrix is type of matrix describing the linear system.
+ */
 template< typename Matrix >
 class TFQMR
 : public LinearSolver< Matrix >
 {
    using Base = LinearSolver< Matrix >;
-public:
-   using RealType = typename Base::RealType;
-   using DeviceType = typename Base::DeviceType;
-   using IndexType = typename Base::IndexType;
-   using VectorViewType = typename Base::VectorViewType;
-   using ConstVectorViewType = typename Base::ConstVectorViewType;
 
-   bool solve( ConstVectorViewType b, VectorViewType x ) override;
+   public:
 
-protected:
-   void setSize( const VectorViewType& x );
+      /**
+       * \brief Floating point type used for computations.
+       */
+      using RealType = typename Base::RealType;
 
-   typename Traits< Matrix >::VectorType d, r, w, u, v, r_ast, Au, M_tmp;
+      /**
+       * \brief Device where the solver will run on and auxillary data will alloacted on.
+       */
+      using DeviceType = typename Base::DeviceType;
+
+      /**
+       * \brief Type for indexing.
+       */
+      using IndexType = typename Base::IndexType;
+
+      /**
+       * \brief Type for vector view.
+       */
+      using VectorViewType = typename Base::VectorViewType;
+
+      /**
+       * \brief Type for constant vector view.
+       */
+      using ConstVectorViewType = typename Base::ConstVectorViewType;
+
+      /**
+       * \brief Method for solving of a linear system.
+       *
+       * See \ref LinearSolver::solve for more details.
+       *
+       * \param b vector with the right-hand side of the linear system.
+       * \param x vector for the solution of the linear system.
+       * \return true if the solver converged.
+       * \return false if the solver did not converge.
+       */
+      bool solve( ConstVectorViewType b, VectorViewType x ) override;
+
+   protected:
+      void setSize( const VectorViewType& x );
+
+      typename Traits< Matrix >::VectorType d, r, w, u, v, r_ast, Au, M_tmp;
 };
 
-} // namespace Linear
-} // namespace Solvers
+      } // namespace Linear
+   } // namespace Solvers
 } // namespace TNL
 
-#include "TFQMR.hpp"
+#include <TNL/Solvers/Linear/TFQMR.hpp>
