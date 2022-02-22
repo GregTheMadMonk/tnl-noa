a6e9a1f77a86c18ecc426aff9c113de19172a5bd
Tomáš Oberhuber <oberhuber.tomas@gmail.com>
2022-02-22 22:59:47 +0100

    Fixing Python script for ODE tutorial.

--- Documentation/Examples/Solvers/ODE/StaticODESolver-SineExample.py
+++ Documentation/Examples/Solvers/ODE/StaticODESolver-SineExample.py
@@ -11,20 +11,19 @@ y_lst = []
 for line in f:
    line = line.strip()
    a = line.split()
-   x_lst.append( a[ 0 ] )
-   y_lst.append( a[ 1 ] )
+   x_lst.append( float( a[ 0 ] ) )
+   y_lst.append( float( a[ 1 ] ) )
 
 x = np.array(x_lst)
 y = np.array(y_lst)
 
-print( x )
-print( y )
+#print( x )
+#print( y )
 
 fig, ax = plt.subplots()
 #ax.set_aspect('auto')
-ax.set_xlim( xmin=0,xmax=10 )
-ax.set_ylim( [0,10] )
-ax.set_xbound(lower=0, upper=10)
+ax.set_xlim( [0,10] )
+ax.set_ylim( [-10,10] )
 #ax.set(xlim=(0,10), xticks=np.arange(1, 10), ylim=(0, 10), yticks=np.arange(1, 10))
 ax.plot(x, y, linewidth=2.0)
 plt.xlim( xmin=0, xmax=10 )
